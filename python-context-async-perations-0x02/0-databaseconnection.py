8e997fd1c0d6782012c933eea925be03152e7760
sweatshirt2 <sweatshirtblue2@gmail.com>
2025-06-01 10:06:31 -0700

    database connection execution manager

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+
+class DatabaseConnection:
+
+  def __init__(self, db_name):
+    self.db_name = db_name
+    self.connection = None
+
+  def __enter__(self):
+    self.connection = sqlite3.connect('')
+    return self.connection
+
+  def __exit__(self, exc_type, exc_value, traceback):
+    if (self.connection):
+      connection.close()
+
+    return False
+
