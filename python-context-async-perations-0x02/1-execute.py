6b801d247cc4408e99c125095c3e86e6a241cfb6
sweatshirt2 <sweatshirtblue2@gmail.com>
2025-06-01 10:15:32 -0700

    execute query

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+
+class ExecuteQuery:
+  def __init__(self, db_name):
+    self.db_name = db_name
+    self.connection = None
+
+  def __enter__(self):
+    self.connection = sqlite3.connect('')
+    return self
+
+  def execute_query(query, param):
+    cursor = None
+    try:
+      cursor = connection.cursor()
+      return cursor.execute(query, (param,))
+    raise sqlite3.Error as e:
+      if cursor:
+        cursor.close()
+
+  def __exit__(self, exc_type, exc_value, traceback):
+    if (self.connection):
+      connection.close()
+
+    return False
+
+
+with ExecuteQuery() as execute_query:
+  query_result = execute_query.execute_query("SELECT * FROM users WHERE age > ?", 25)
+
