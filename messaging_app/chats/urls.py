1574fe1929f302f925c3df0c10e393be3eb9ed81
sweatshirt <sweatshirtblue2@gmail.com>
2025-06-08 21:14:38 +0300

    fix: urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, include  # Todo: explore how include can be used here
 from .views import (
     UserProfileListCreateAPIView,
     ConversationListCreateAPIView,
     MessageListCreateAPIView,
 )
+
+# Todo: explore how these could be used here
+# from rest_framework import routers
+# routers.DefaultRouter()
 
 urlpatterns = [
     path("users", UserProfileListCreateAPIView.as_view(), name="users_list_create"),
