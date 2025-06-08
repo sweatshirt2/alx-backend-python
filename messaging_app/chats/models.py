098e2e074d1c5c0efd9720de7a6a8653f3ff31c8
sweatshirt <sweatshirtblue2@gmail.com>
2025-06-08 16:03:24 +0300

    fix: user model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -18,6 +18,9 @@ class UserProfile(AbstractUser):
 
 
 class Conversation(models.Model):
+    conversation_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
     participants = models.ManyToManyField(UserProfile)
     created_at = models.DateTimeField(auto_now_add=True)
 
