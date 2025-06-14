6b54f62370919e4c7a82cc64c549ba35e6bedc61
sweatshirt <sweatshirtblue2@gmail.com>
2025-06-14 15:04:57 +0300

    week-6: tasks-0 comment docs

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -6,5 +6,14 @@ from .models import Message, Notification
 
 @receiver(post_save, sender=Message)
 def message_received(sender, instance, created, **kwargs):
+    """triggers creating a notification when a message is sent to a user
+
+    Kwargs:
+    instance -- an instance of message
+    created -- whether the message is new
+
+    Return: return_description
+    """
+
     if created:
         Notification.objects.create(message=instance, user=instance.receiver)
